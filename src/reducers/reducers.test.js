be6e030e26eb1a9dbf279633bfbef6ed71e7af0d
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-11-02 13:46:38 -0600

    Attempt to test reducers

---
+++ src/reducers/reducers.test.js
@@ -0,0 +1,33 @@
+import houseData from '../reducers/GetDataReducer';
+import * as actions from '../actions/index';
+
+describe('getData reducer tests', () => {
+  it('should return our initial state', () => {
+    const expectation = [];
+
+    expect(houseData(undefined, {})).toEqual(expectation);
+  });
+
+// this test is failing because i'm getting an object back;
+// i don't know why i'm getting an object back when houseData is an array with one object in it
+  it('should populate state with house data', () => {
+    const action = {
+      type: 'GET_HOUSE_DATA',
+      houseData: [
+        {
+          name: "House Corbray of Heart's Home",
+          founded: '',
+          seats: ["Heart's Home"],
+          titles: ["Lord of Heart's Home", "King of the Fingers (historical)"],
+          coatOfArms: 'Three black ravens in flight, holding three red hearts, on a white field(Argent, three ravens volant sable, each clutching in their claws a heart gules)',
+          ancestralWeapons: ["Lady Forlorn"],
+          words: ''
+        }
+      ]
+    };
+
+    const expectation = [action.houseData];
+
+    expect(houseData(undefined, action)).toEqual(expectation);
+  });
+});
