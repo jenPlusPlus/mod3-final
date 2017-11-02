6dacf0e73f8653708868ddc6525ff7c7a5795a70
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-11-02 13:15:58 -0600

    attempt to pass a single test

---
+++ src/components/App/App.test.js
@@ -0,0 +1,22 @@
+import { shallow } from 'enzyme';
+import { App } from './App';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './../../reducers/root-reducer';
+
+
+describe('App snapshot', () => {
+ // getting error:  Enzyme Internal Error: unknown composite type undefined
+  it.skip('should always match the snapshot', () => {
+    const store = createStore(rootReducer, applyMiddleware(thunk));
+    const wrapper = shallow(
+      <Provider store={store}>
+        <App />
+      </Provider>);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
