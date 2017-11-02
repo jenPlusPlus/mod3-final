d97c740d6e5fc46d8b6ee52226ba12d7ae93f165
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-11-02 11:42:39 -0600

    add seats and founded to cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Card extends Component {
   constructor() {
     super();
+  }
+
+  getFounded() {
+    const foundedWhat = this.props.house.founded === ''
+      ? 'None'
+      : this.props.house.founded;
+    return foundedWhat;
+  }
+
+  getSeats() {
+    const mappedSeats = this.props.house.seats.map((seat, index) => {
+      return (
+        <li key={index}>{seat}</li>
+      );
+    })
+    return mappedSeats
   }
 //name, founded, seats, titles, coatOfArms, ancestralWeapons, words
   render() {
@@ -11,8 +28,16 @@ class Card extends Component {
     return (
       <div className='card'>
         <h3>{house.name}</h3>
+        <p>Founded: {this.getFounded()}</p>
+        <ul>Seats:
+          {this.getSeats()}
+        </ul>
       </div>);
   }
 }
+
+Card.propTypes = {
+  house: PropTypes.object
+};
 
 export default Card;
