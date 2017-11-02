48e566667c94da9c605cf75467aac3af8a75d9fd
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-11-02 11:54:46 -0600

    add all remaining data to cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,6 +6,50 @@ class Card extends Component {
     super();
   }
 
+  // the next three functions could be refactored into one
+  getSeats() {
+    let mappedSeats;
+    if (this.props.house.seats.length > 0) {
+      mappedSeats = this.props.house.seats.map((seat, index) => {
+        return (
+          <li key={index}>{seat}</li>
+        );
+      });
+    } else {
+      mappedSeats = <li key='1'>None</li>;
+    }
+    return mappedSeats;
+  }
+
+  getTitles() {
+    let mappedTitles;
+    if (this.props.house.titles.length > 0){
+      mappedTitles = this.props.house.titles.map((title, index) => {
+        return (
+          <li key={index}>{title}</li>
+        );
+      });
+    } else {
+      mappedTitles = <li key='1'>None</li>;
+    }
+    return mappedTitles;
+  }
+
+  getWeapons() {
+    let mappedWeapons;
+    if (this.props.house.ancestralWeapons.length > 0){
+      mappedWeapons = this.props.house.ancestralWeapons.map((weapon, index) => {
+        return (
+          <li key={index}>{weapon}</li>
+        );
+      });
+    } else {
+      mappedWeapons = <li key='1'>None</li>;
+    }
+    return mappedWeapons;
+  }
+
+  // the next two functions could be refactored into one
   getFounded() {
     const foundedWhat = this.props.house.founded === ''
       ? 'None'
@@ -13,18 +57,15 @@ class Card extends Component {
     return foundedWhat;
   }
 
-  getSeats() {
-    const mappedSeats = this.props.house.seats.map((seat, index) => {
-      return (
-        <li key={index}>{seat}</li>
-      );
-    })
-    return mappedSeats
+  getWords() {
+    const words = this.props.house.words === ''
+      ? 'None'
+      : this.props.house.words;
+    return words;
   }
-//name, founded, seats, titles, coatOfArms, ancestralWeapons, words
+
   render() {
     const { house } = this.props;
-    console.log('props: ', house);
     return (
       <div className='card'>
         <h3>{house.name}</h3>
@@ -32,6 +73,14 @@ class Card extends Component {
         <ul>Seats:
           {this.getSeats()}
         </ul>
+        <ul>Titles:
+          {this.getTitles()}
+        </ul>
+        <p>Coat of Arms: {house.coatOfArms}</p>
+        <ul>Ancestral Weapons:
+          {this.getWeapons()}
+        </ul>
+        <p>Words: {this.getWords()}</p>
       </div>);
   }
 }
